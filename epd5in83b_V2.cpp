bbf035c8b84351c75142a5da0c77165c500d44eb
Christoph Knaden <cknaden@uni-muenster.de>
2023-06-21 15:42:55 +0200

    update for 2 images with each 2 layers

--- epd5in83b_V2.cpp
+++ epd5in83b_V2.cpp
@@ -28,51 +28,50 @@
 #include "epd5in83b_V2.h"
 #include "imagedata.h"
 
-Epd::~Epd() {
-};
+Epd::~Epd(){};
 
 Epd::Epd() {
-    reset_pin = RST_PIN;
-    dc_pin = DC_PIN;
-    cs_pin = CS_PIN;
-    busy_pin = BUSY_PIN;
-    width = EPD_WIDTH / 8;
-    height = EPD_HEIGHT;
+  reset_pin = RST_PIN;
+  dc_pin = DC_PIN;
+  cs_pin = CS_PIN;
+  busy_pin = BUSY_PIN;
+  width = EPD_WIDTH / 8;
+  height = EPD_HEIGHT;
 };
 
 int Epd::Init(void) {
   if (IfInit() != 0) {
-      return -1;
+    return -1;
   }
   Reset();
 
-  SendCommand(0x01);      //POWER SETTING
-  SendData (0x07);
-  SendData (0x07);    //VGH=20V,VGL=-20V
-  SendData (0x3f);    //VDH=15V
-  SendData (0x3f);    //VDL=-15V
+  SendCommand(0x01);  //POWER SETTING
+  SendData(0x07);
+  SendData(0x07);  //VGH=20V,VGL=-20V
+  SendData(0x3f);  //VDH=15V
+  SendData(0x3f);  //VDL=-15V
 
-  SendCommand(0x04); //POWER ON
-  DelayMs(100);  
-  WaitUntilIdle();        //waiting for the electronic paper IC to release the idle signal
+  SendCommand(0x04);  //POWER ON
+  DelayMs(100);
+  WaitUntilIdle();  //waiting for the electronic paper IC to release the idle signal
 
-  SendCommand(0X00);      //PANNEL SETTING
-  SendData(0x0F);   //KW-3f   KWR-2F  BWROTP 0f BWOTP 1f
+  SendCommand(0X50);  //PANNEL SETTING
+  SendData(0x0F);     //KW-3f   KWR-2F  BWROTP 0f BWOTP 1f
 
-  SendCommand(0x61);          //tres      
-  SendData (0x02);    //source 648
-  SendData (0x88);
-  SendData (0x01);    //gate 480
-  SendData (0xe0);
+  SendCommand(0x61);  //tres
+  SendData(0x02);     //source 648
+  SendData(0x88);
+  SendData(0x01);  //gate 480
+  SendData(0xe0);
 
-  SendCommand(0X15);    
-  SendData(0x00);   
+  SendCommand(0X15);
+  SendData(0x00);
 
-  SendCommand(0X50);      //VCOM AND DATA INTERVAL SETTING
+  SendCommand(0X50);  //VCOM AND DATA INTERVAL SETTING
   SendData(0x11);
   SendData(0x07);
 
-  SendCommand(0X60);      //TCON SETTING
+  SendCommand(0X60);  //TCON SETTING
   SendData(0x22);
   return 0;
 }
@@ -81,16 +80,16 @@ int Epd::Init(void) {
  *  @brief: basic function for sending commands
  */
 void Epd::SendCommand(unsigned char command) {
-    DigitalWrite(dc_pin, LOW);
-    SpiTransfer(command);
+  DigitalWrite(dc_pin, LOW);
+  SpiTransfer(command);
 }
 
 /**
  *  @brief: basic function for sending data
  */
 void Epd::SendData(unsigned char data) {
-    DigitalWrite(dc_pin, HIGH);
-    SpiTransfer(data);
+  DigitalWrite(dc_pin, HIGH);
+  SpiTransfer(data);
 }
 
 /**
@@ -98,13 +97,11 @@ void Epd::SendData(unsigned char data) {
  */
 void Epd::WaitUntilIdle(void) {
   UBYTE busy;
-  do
-  {
+  do {
     SendCommand(0x71);
     busy = DigitalRead(busy_pin);
-    busy =!(busy & 0x01);
-  }
-  while(busy);
+    busy = !(busy & 0x01);
+  } while (busy);
 }
 
 /**
@@ -113,126 +110,124 @@ void Epd::WaitUntilIdle(void) {
  *          see Epd::Sleep();
  */
 void Epd::Reset(void) {
-    DigitalWrite(reset_pin, LOW);                //module reset    
-    DelayMs(1);
-    DigitalWrite(reset_pin, HIGH);
-    DelayMs(200);    
+  DigitalWrite(reset_pin, LOW);  //module reset
+  DelayMs(1);
+  DigitalWrite(reset_pin, HIGH);
+  DelayMs(200);
 }
 
 void Epd::DisplayFrame(const UBYTE *blackimage, const UBYTE *ryimage) {
-    SendCommand(0x10);
-    for (UWORD j = 0; j < height; j++) {
-        for (UWORD i = 0; i < width; i++) {
-          SendData(pgm_read_byte(&blackimage[i + (j*width)]));
-        }
-    }   
-    SendCommand(0x13);
-    for (UWORD j = 0; j < height; j++) {
-        for (UWORD i = 0; i < width; i++) {
-          SendData(pgm_read_byte(&ryimage[i + (j*width)]));
-        }
+  SendCommand(0x10);
+  for (UWORD j = 0; j < height; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(pgm_read_byte(&blackimage[i + (j * width)]));
     }
-    SendCommand(0x12);
-    WaitUntilIdle();
+  }
+  SendCommand(0x13);
+  for (UWORD j = 0; j < height; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(pgm_read_byte(&ryimage[i + (j * width)]));
+    }
+  }
+  SendCommand(0x12);
+  WaitUntilIdle();
 }
 
-void Epd::DisplayPictureDemo(const UBYTE *blackimage, UBYTE Block)
-{
+void Epd::DisplayPictureDemo(const UBYTE *blackimage, UBYTE Block) {
   UDOUBLE i, j;
-  if(Block == 0){
+  if (Block == 0) {
     Serial.println("-- IMAGE_DATA 0 --");
     SendCommand(0x10);
-    for (j = 0; j < height/2; j++) {
-        for (i = 0; i < width; i++) {
-            SendData(pgm_read_byte(&blackimage[i + j * width]));
-        }
+    for (j = 0; j < height / 2; j++) {
+      for (i = 0; i < width; i++) {
+        SendData(pgm_read_byte(&blackimage[i + j * width]));
+      }
     }
-    
-  }else if(Block == 1){
+
+  } else if (Block == 1) {
     Serial.println("-- IMAGE_DATA 1 --");
-    for (j = 0; j < height/2; j++) {
-        for (i = 0; i < width; i++) {
-            SendData(pgm_read_byte(&blackimage[i + j * width]));
-        }
+    for (j = 0; j < height / 2; j++) {
+      for (i = 0; i < width; i++) {
+        SendData(pgm_read_byte(&blackimage[i + j * width]));
+      }
     }
-  }else if(Block == 2){
+  } else if (Block == 2) {
     Serial.println("-- IMAGE_DATA 2 --");
     SendCommand(0x13);
-    for (j = 0; j < height/2; j++) {
-        for (i = 0; i < width; i++) {
-            SendData(pgm_read_byte(&blackimage[i + j * width]));
-        }
+    for (j = 0; j < height / 2; j++) {
+      for (i = 0; i < width; i++) {
+        SendData(pgm_read_byte(&blackimage[i + j * width]));
+      }
     }
-  }else if(Block == 3){
+  } else if (Block == 3) {
     Serial.println("-- IMAGE_DATA 3 --");
-    for (j = 0; j < height/2; j++) {
-        for (i = 0; i < width; i++) {
-            SendData(pgm_read_byte(&blackimage[i + j * width]));
-        }
+    for (j = 0; j < height / 2; j++) {
+      for (i = 0; i < width; i++) {
+        SendData(pgm_read_byte(&blackimage[i + j * width]));
+      }
     }
     SendCommand(0x12);
     WaitUntilIdle();
   }
 }
 
-void Epd::DisplayPicture(const UBYTE *upperImage, const UBYTE *lowerImage)
-{
+void Epd::DisplayPicture(const UBYTE *upperImage_Layer1, const UBYTE *upperImage_Layer2, const UBYTE *lowerImage_Layer1, const UBYTE *lowerImage_Layer2) {
   Serial.println("-- DisplayPicture(): Start --");
   Serial.println("-- DisplayPicture(): Start GeneratePictureUpperHalf() --");
-  GeneratePictureUpperHalf(upperImage);
+  GeneratePictureUpperAndLowerHalf(upperImage_Layer1, upperImage_Layer2, lowerImage_Layer1, lowerImage_Layer2);
   Serial.println("-- DisplayPicture(): Start GeneratePictureLowerHalf() --");
-  GeneratePictureLowerHalf(lowerImage);
-  
-  SendCommand(0x12);
+
+  SendCommand(0x12); // Combine to picture; After generation of pictures
   Serial.println("-- DisplayPicture(): Start WaitUntilIdle() --");
   WaitUntilIdle();
   Serial.println("-- DisplayPicture(): Finished WaitUntilIdle() --");
   Serial.println("-- DisplayPicture(): Finished --");
 }
 
-void Epd::GeneratePictureUpperHalf(const UBYTE *blackimage)
-{
+void Epd::GeneratePictureUpperAndLowerHalf(const UBYTE *upperImage_Layer1, const UBYTE *upperImage_Layer2, const UBYTE *lowerImage_Layer1, const UBYTE *lowerImage_Layer2) {
   UDOUBLE i, j;
-  SendCommand(0x10);
-  for (j = 0; j < height/2; j++) {
-      for (i = 0; i < width; i++) {
-          SendData(pgm_read_byte(&blackimage[i + j * width]));
-      }
+  SendCommand(0x10); // interpret as black / white data
+  for (UWORD j = 0; j < height/2; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(pgm_read_byte(&upperImage_Layer1[i + (j * width)]));
+    }
+  }
+  for (UWORD j = 0; j < height/2; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(pgm_read_byte(&lowerImage_Layer1[i + (j * width)]));
+    }
+  }
+  SendCommand(0x13); // interpret as red / white data
+  for (UWORD j = 0; j < height/2; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(pgm_read_byte(&upperImage_Layer2[i + (j * width)]));
+    }
+  }
+  for (UWORD j = 0; j < height/2; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(pgm_read_byte(&lowerImage_Layer2[i + (j * width)]));
+    }
   }
 }
-
-void Epd::GeneratePictureLowerHalf(const UBYTE *blackimage)
-{
-  UDOUBLE i, j;
-  for (j = 0; j < height/2; j++) {
-      for (i = 0; i < width; i++) {
-          SendData(pgm_read_byte(&blackimage[i + j * width]));
-      }
-  }
-}
-
 
 void Epd::Clear(void) {
-    Serial.println("-- Clear(): Start --");
-    //send black data
-    SendCommand(0x10);
-    for (UWORD j = 0; j < height; j++) {
-        for (UWORD i = 0; i < width; i++) {
-            SendData(0xff);
-        }
+  Serial.println("-- Clear(): Start --");
+  //send black data
+  SendCommand(0x10);
+  for (UWORD j = 0; j < height; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(0xff);
     }
-
-    //send red data
-    SendCommand(0x13);
-    for (UWORD j = 0; j < height; j++) {
-        for (UWORD i = 0; i < width; i++) {
-            SendData(0x00);
-        }
+  }
+  SendCommand(0x13);
+  for (UWORD j = 0; j < height; j++) {
+    for (UWORD i = 0; i < width; i++) {
+      SendData(0x00);
     }
-    
-    SendCommand(0x12);
-    WaitUntilIdle();
-    Serial.println("-- Clear(): Finished --");
+  }
+  SendCommand(0x12);
+  WaitUntilIdle();
+  Serial.println("-- Clear(): Finished --");
 }
 
 /**
@@ -243,12 +238,8 @@ void Epd::Clear(void) {
  *          You can use EPD_Reset() to awaken
  */
 void Epd::Sleep(void) {
-    SendCommand(0x02); // POWER_OFF
-    WaitUntilIdle();
-    SendCommand(0x07); // DEEP_SLEEP
-    SendData(0xA5); // check code
+  SendCommand(0x02);  // POWER_OFF
+  WaitUntilIdle();
+  SendCommand(0x07);  // DEEP_SLEEP
+  SendData(0xA5);     // check code
 }
-
-
-
-/* END OF FILE */
